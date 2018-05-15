60201333797728504304aff7bebb34c4dbb8c908
VikasTelukunta <telukunta.vikas@gmail.com>
2018-05-14 22:57:17 -0400

    Third commit

--- JenkinsGITHUB/src/GitHubClass.java
+++ JenkinsGITHUB/src/GitHubClass.java
@@ -3,7 +3,8 @@ public class GitHubClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Hi, lets run this script through Jenkins");
+		
 	}
 
 }
